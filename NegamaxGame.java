c6de1e2d0a7105f87ac6b250c27ce670b4f33956
Zsolt Pajor-Gyulai <pgyzs1@gmail.com>
2018-02-24 17:35:21 -0500

    New files

---
+++ NegamaxGame.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class NegamaxGame extends Game {
+	int[] savedLastMove;
+	// Implements the negamax tree search algorithm.
+	public NegamaxGame() {}
+	public NegamaxGame(int[] dimsOfBoard, int startingPlayer) {
+		super(dimsOfBoard, startingPlayer);
+	}
+	@Override
+	protected Value valueOfMove(int k, int l, boolean keepLastMove) {
+		/* Returns the value gained by the active agent of the move (k,l).
+		   It checks whether the move ends the game and if not then recursively 
+		   tries out all the possible moves. The implementation needs to make 
+		   sure that the game status is the same coming in and out of the 
+		   method. If the last argument is true, the current LastMove is saved
+		   and restored in the end.*/
+		if (keepLastMove) 
+			savedLastMove = lastMove.clone();
+		Value value;  // Acts as -infty
+		// Make the move
+		lastMove[0] = k;
+		lastMove[1] = l;
+		board[k][l] = activeAgent;
+		movesCompleted += 1;
+		// Evaluate the move
+		if (winOnLastMove()) {
+			value = new Value(1, movesCompleted);
+		}
+		else if (isFull()) {
+			value = new Value(0, movesCompleted);
+		}
+		else {
+			// Recursively evaluate move (i,j) for opponent and
+			// find the best one from his/her perspective
+			activeAgent = 3 - activeAgent;
+			value = new Value(-2,0); // Corresponds to -infty
+			Value tempValue;
+			for(int i = 0; i < board.length; i++) {
+				for(int j = 0; j < board[i].length; j++) {
+					if (board[i][j] == 0) {
+						tempValue = valueOfMove(i, j, false);
+						if (tempValue.compareTo(value) == 1)
+							value = tempValue;
+					}
+				}
+			}
+			activeAgent = 3 - activeAgent;
+			// The value of the position is the negative of the best value
+			// for the opponent.
+			value.flipValue();
+		}
+		// Undo the move
+		if (keepLastMove)
+			lastMove = savedLastMove;
+		board[k][l] = 0;
+		movesCompleted -= 1;
+		return value;
+	}
+}
---
+++ NegamaxGameMemoized.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class NegamaxGameMemoized extends NegamaxGame {
+	protected HashMap<Integer, int[]> database;
+
+	public NegamaxGameMemoized(int[] dimsOfBoard, int startingPlayer) {
+		database = new HashMap<Integer, int[]>();
+		scoreBoard = new int[] {0,0};
+		board = new int[dimsOfBoard[0]][dimsOfBoard[1]];
+		lastMove = new int[] {-1, -1};
+		setVictoryCondition();
+		restart(startingPlayer);
+	}
+	@Override
+	protected Value valueOfMove(int k, int l, boolean keepLastMove) {
+		int key = genKey(k,l);
+		if (database.containsKey(key)) {
+			int[] entry = database.get(key);
+			return new Value(entry[0], entry[1]);
+		}
+		Value value = super.valueOfMove(k,l, keepLastMove);
+		database.put(key, new int[] {value.outcome, value.lengthOfGame});
+		return value;
+	}
+	protected int genKey(int k, int l) {
+		/* Generates a unique integer based on the state of the game. Since all 
+		   entries are either 0,1,2, this integer is the ternary representation.
+		   of the board in a row major order*/
+		int key = 0;
+		board[k][l] = activeAgent;
+		for (int i=0; i < board.length; i++)
+			for(int j=0; j < board[i].length; j++)
+				key = 3 * key + board[i][j];
+		key = 3 * key + activeAgent;
+		board[k][l] = 0;
+		return key;
+	}
+}
